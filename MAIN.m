f30751dafbb022f434fd9564ba0bd9e6e01cb9ef
Nathalia Belén Muñoz Durán <76854563+nbelenmunoz@users.noreply.github.com>
2025-11-25 15:56:03 +0100

    Update MAIN.m
    Last routine that work

--- MAIN.m
+++ MAIN.m
@@ -1,18 +1,22 @@
-close all; 
+close all;
 clear all;
 clc;
+
 %% DECIDO QUALI TOOL USARE
-%Simulation or online 
+%Simulation or online
 simulo=0;
 if simulo==1
-    addr=[1,1,1,1]; %non genero address ma vado avanti nei blocchi di programma
+    addr=[1,1]; % Only robot1 and extruder addresses
 end
 %Metto 0 o 1 se non uso/uso il dispositivo
-robot1=1; robot2=0; estrusore=1; plc=0;
+robot1=1; estrusore=0; % Removed robot2 and plc
 %per modificare i loop e tenerli corretti nel teaching ai soli robot
-toolvar=estrusore+plc;
-%Per sapere il dispositivo i-esimo che numero è effettuo una cumsum di 1 
-toolvec=[robot1,robot1+robot2];toolvec=[toolvec,toolvec(end)+estrusore];toolvec=[toolvec,toolvec(end)+plc];
+toolvar=estrusore; % Removed plc
+%Per sapere il dispositivo i-esimo che numero è effettuo una cumsum di 1
+toolvec=robot1; % Only robot1
+if estrusore==1
+    toolvec=[toolvec,toolvec(end)+estrusore]; % Add extruder if enabled
+end
 
 %% GENERAZIONE ADDRESS (CHECK ROBOT 1 ID AND PORT)
 clc;
@@ -20,25 +24,17 @@ if simulo==0
     %vettore delle connessioni
     t=[];
     tout=20;conntout=30;%secondi
-    %address e port di 8CRL,FRB,ESTRUSORE,PLC
+    %address e port di ROBOT1 e ESTRUSORE
     addr=[]; port=[];
-    if robot1==1 %8CRL (GRANDE) e 5ASD (assista)
-        %addr=[addr,"127.0.0.1"];
+    if robot1==1 %ROBOT1
         addr=[addr,"192.168.0.22"];
-        port=[port,10008];
-    end
-    if robot2==1 %FRB (PICCOLO)
-        addr=[addr,"192.168.0.35"];
         port=[port,10008];
     end
     if estrusore==1 %ESTRUSORE
         addr=[addr,"192.168.0.6"];
         port=[port,1000];
     end
-    if plc==1 %PLC
-        addr=[addr,"192.168.0.42"];
-        port=[port,10002];
-    end
+    
     %Connetto
     if length(addr)<1 %In caso nesun dispositivo sia dichiarato
         error("NO DEVICE DECLARED");
@@ -46,7 +42,7 @@ if simulo==0
         for i=1:length(addr) %Generazione del vettore connessioni
             t=[t,ADDRESS(addr(i),port(i),tout,conntout)];
             if i==1
-                configureTerminator(t(i),"CR"); %assista ha bisogno di CR specificato
+                configureTerminator(t(i),"CR"); %robot1 ha bisogno di CR specificato
             end
         end
     end
@@ -54,12 +50,14 @@ end
 
 %% GENERATE HERE THE POINTS MATRIX
 % X Y Z A B C Vrobot Varduino(200-1000) Tdelay(delay time the robot waits before moving to the next point)
+%X Y Z A B C EXTRUDER
 M = [
-   250.00,   0.00,    500.000, -180.00,  0.00,  0.00,   20, 0,   1;
-   230.000,  0.000,   330.000, -180.000, 0.000, 0.000,  20, 0,   3;  % P1S
-   230.000,  0.000,   175.000, -180.000, 0.000, 0.000,  20, 500, 1;  % P1
-   400.000,  0.000,   175.000, -180.000, 0.000, 0.000,  20, 200, 2;  % P2
-   400.000,  0.000,   330.000, -180.000, 0.000, 0.000,  20, 0,   3;  % P2S
+    315.000,  -10.000, 470.000, -180.000, 0.000, 0.000, 10, 0,     1; % P1S
+    315.000,  -10.000, 415.000, -180.000, 0.000, 0.000, 20, 0,     2;
+    315.000,  -10.000, 415.000, -180.000, 0.000, 0.000, 30, 40,    2;% P1
+    315.000,   60.000, 415.000, -180.000, 0.000, 0.000, 40, 40,    2; % P2
+    315.000,   60.000, 470.000, -180.000, 0.000, 0.000, 50, 0,     0;
+    315.000,   60.000, 470.000, -180.000, 0.000, 0.000, 60, 0,     0;% P2S
 ];
 
 %Save this matrix as "Points"
@@ -71,181 +69,147 @@ clc;
 nbuff=1;
 %load Points.mat;
 PPnum=Points(:,1:6);
-vel=Points(:,7:7+toolvec(end)-toolvar); vel(1,:)=50; %correzione vel avvicinamento a P1
-toolvel=Points(:,end-1-toolvar:end-1); toolvel(1,:)=0;%correzione tool vel avvicinamento a P1
-delaytime=Points(:,end); 
+vel=Points(:,7:7); % Only robot1 velocity
+vel(1,:)=50; %correzione vel avvicinamento a P1
+toolvel=Points(:,end-1:end-1); % Only extruder velocity
+toolvel(1,:)=0;%correzione tool vel avvicinamento a P1
+delaytime=Points(:,end);
 np=length(PPnum(:,1));%numero finale di punti
+
 %Approssimo
 approx=2;
 PPnum=round(PPnum,2);vel=round(vel,approx);delaytime=round(delaytime,2);
+
 %CONVERTO A STRINGHE CORRETTE PER IL TEACHING
 for i=1:length(PPnum(:,1))
-    for k=1:toolvec(end)-toolvar
-        if k==1
-            flag="(6,0)"; %flag robot 1
-        elseif k==2
-            flag="(7,0)"; %flag robot 2
-        end
+    for k=1:1 % Only robot1
+        flag="(6,0)"; %flag robot 1
         PPstr(i,k)=genP(PPnum(i,:,k),flag); %stringa punti robot
     end
 end
-if estrusore==1 
+
+if estrusore==1
     extrstr=[];
     for i=1:length(PPnum(:,1,1))
         extrstr=[extrstr;strcat("G",num2str(toolvel(i,1)),"n")];
-    end
-end
-if plc==1 
-    plcstr=[];
-    for i=1:length(PPnum(:,1,1))
-        plcstr=[plcstr;PLCCONV(toolvel(i,2))];
     end
 end
 
 %% PREPARAZIONE tool
 clc;
 omega = (pi/4);
-extrvel=(10^6)/(4*254.77*omega);
-if estrusore==1 
+extrvel=(10^6)/(4254.77*omega);
+if estrusore==1
     temperatura=0;%gradi centigradi
     tempospurgo=2*pi/omega;%secondi, con -1000 uso 6.3 sec
     gspurgo=extrvel;%velocità spurgo
-    data=EXTRSTART(t(toolvec(3)),temperatura,tempospurgo,gspurgo);
+    data=EXTRSTART(t(toolvec(2)),temperatura,tempospurgo,gspurgo); %Activacion motor
 end
-%%
-clc;
-if plc==1
-    data=PLCSTART(t(toolvec(4)));
-end
+
 %Suggerimenti
-if toolvec(end)-toolvar>0 %Se i robot esistono
+if robot1>0 %Se il robot esiste
     fprintf("Be sure that PP and MVEL on Mitsubishi are at least long as nbuff(%i) \n",nbuff);
-    disp("Servo On, Start MOV1 and MOV2 on the robots.");
-    disp("Wait the movement finish of the first robot to move the second etc.");
+    disp("Servo On, Start MOV1 and MOV2 on the robot.");
 end
 
 %% TEACHING DEI PRIMI PUNTI
-if toolvec(end)-toolvar>0 %Se i robot esistono
-    %teach np to the robots
-    for i=1:toolvec(end)-toolvar
-        writeline(t(i),num2str(np)); %scrivo np
-    end
+if robot1>0 %Se il robot esiste
+    %teach np to the robot
+    writeline(t(1),num2str(np)); %scrivo np
     pause(0.2);
-    for i=1:toolvec(end)-toolvar
-        writeline(t(i),num2str(nbuff)); %scrivo nbuffer
-    end
-    %teach P1-Pk to the robots
+    writeline(t(1),num2str(nbuff)); %scrivo nbuffer
+    
+    %teach P1-Pk to the robot
     pause(1);
-    for i=1:toolvec(end)-toolvar
-        for k=1:nbuff
-            str="P-fatto";
-            %data=TEACH(t(i),PPstr(k,i),num2str(vel(k,i)),str);
-            write(t(i),PPstr(k,i));
-            pause(0.5);
-            write(t(i),num2str(vel(k,i)))
-            pause(1);
-        end
+    for k=1:nbuff
+        str="P-fatto";
+        %data=TEACH(t(1),PPstr(k,1),num2str(vel(k,1)),str);
+        write(t(1),PPstr(k,1));
+        pause(0.5);
+        write(t(1),num2str(vel(k,1)))
+        pause(1);
     end
+    
     pause(0.5);
-    %robots move to P1
-    for i=1:toolvec(end)-toolvar
-        writeline(t(i),num2str(0));
-    end
-    for i=1:toolvec(end)-toolvar
-        temp=CONTROL(t(i),str);
-        fprintf("ROBOT %i: %s \n",i,temp);
-    end
+    %robot moves to P1
+    writeline(t(1),num2str(0));
+    temp=CONTROL(t(1),str);
+    fprintf("ROBOT 1: %s \n",temp);
 end
 
 %% PROCESSO
-%https://it.mathworks.com/help/parallel-computing/quick-start-parallel-computing-in-matlab.html
-% parfor i = 1:3
-%     c(i) = max(eig(rand(1000)));
-% end
 k=1;
-data=strings(length(PPnum(:,1,1)),toolvec(end)-toolvar);
+data=strings(length(PPnum(:,1,1)),1); % Only for robot1
 tempovec=zeros(length(PPnum(:,1,1)),length(addr));
+
 if estrusore==1 %Parte estrusore
-    write(t(toolvec(3)),extrstr(2,:));
-    tempovec(k,toolvec(3))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
+    write(t(toolvec(2)),extrstr(2,:));
+    tempovec(k,toolvec(2))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
 end
-if plc==1 %Parte PLC
-    write(t(toolvec(4)),plcstr(2,:));
-    tempovec(k,toolvec(4))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
-end
+
 %CONTROLLO MOVIMENTO
-if toolvec(end)-toolvar>0 %Se i robot esistono
+if robot1>0 %Se il robot esiste
     pause(delaytime(1));
-    for i=1:toolvec(end)-toolvar
-        writeline(t(i),num2str(1));
-        data(1,i)=CONTROL(t(i),str);
-        tempovec(1,i)= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
-        fprintf("P-%i \n",1);
-    end
+    writeline(t(1),num2str(1));
+    data(1,1)=CONTROL(t(1),str);
+    tempovec(1,1)= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
+    fprintf("P-%i \n",1);
 end
+
 for k=nbuff+1:length(PPnum(:,1,1))-1
-    for i=1:toolvec(end)-toolvar %Aggiorno ROBOT, che continuano
-        writeline(t(i),PPstr(k,i));
-        writeline(t(i),num2str(vel(k,i)));
+    if robot1>0 %Aggiorno ROBOT
+        writeline(t(1),PPstr(k,1));
+        writeline(t(1),num2str(vel(k,1)));
     end
-    for i=1:toolvec(end)-toolvar %Ricevo avvenuto arrivo a destinazione dei robot
-        data(k-nbuff+1,i)=CONTROL(t(i),str);
-        tempovec(k-nbuff+1,i)= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
+    
+    if robot1>0 %Ricevo avvenuto arrivo a destinazione del robot
+        data(k-nbuff+1,1)=CONTROL(t(1),str);
+        tempovec(k-nbuff+1,1)= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
     end
+    
     fprintf("P-%i \n",k-nbuff+1);
+    
     if estrusore==1 %Aggiorno velocità estrusore
-        write(t(toolvec(3)),extrstr(k-nbuff+1,:));
-        tempovec(k-nbuff+1,toolvec(3))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
+        write(t(toolvec(2)),extrstr(k-nbuff+1,:));
+        tempovec(k-nbuff+1,toolvec(2))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
     end
-    if plc==1 %Aggiorno velocità PLC
-        write(t(toolvec(4)),plcstr(k-nbuff+1,:));
-        tempovec(k-nbuff+1,toolvec(4))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
-    end
-    if toolvec(end)-toolvar==0 %Se i robot non esistono
-        pause(1);
-    elseif toolvec(end)-toolvar >0 %Se i robot esistono
+    
+    if robot1>0 %Se il robot esiste
         if k==length(PPnum(:,1,1))
-            data(k-nbuff+1,i)=CONTROL(t(i),str);
+            data(k-nbuff+1,1)=CONTROL(t(1),str);
         end
         pause(delaytime(k-nbuff+1));
-        writeline(t(i),num2str(1));
+        writeline(t(1),num2str(1))
     end
 end
+
 for k=2:nbuff
     if estrusore==1 %Aggiorno velocità estrusore
-        write(t(toolvec(3)),extrstr(length(PPnum(:,1,1))-nbuff+k,:));
-        tempovec(length(PPnum(:,1,1))-nbuff+k,toolvec(3))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
+        write(t(toolvec(2)),extrstr(length(PPnum(:,1,1))-nbuff+k,:));
+        tempovec(length(PPnum(:,1,1))-nbuff+k,toolvec(2))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
     end
-    if plc==1 %Aggiorno velocità PLC
-        write(t(toolvec(4)),plcstr(length(PPnum(:,1,1))-nbuff+k,:));
-        tempovec(length(PPnum(:,1,1))-nbuff+k,toolvec(4))= seconds(duration(string(datetime('now','Format','HH:mm:ss.SS'))));
-    end
-    data(length(PPnum(:,1,1))-nbuff+k,i)=CONTROL(t(i),str);
+    
+    data(length(PPnum(:,1,1))-nbuff+k,1)=CONTROL(t(1),str);
     fprintf("P-%i \n",length(PPnum(:,1,1))-nbuff+k);
     pause(delaytime(length(PPnum(:,1,1))-nbuff+k));
 end
+
 %%FINE DEL PROCESSO
-if plc==1 %fermo PLC
-    temp=PLCSTOP(t(toolvec(4)));
-end
 if estrusore==1 %fermo estrusore
-    write(t(toolvec(3)),"G0n");
+    write(t(toolvec(2)),"G0n");
     pause(0.02);
-    write(t(toolvec(3)),"G0n");%lo ripeto per sicurezza
+    write(t(toolvec(2)),"G0n");%lo ripeto per sicurezza
 end
-fprintf("PROCESSO TERMINATO \n",k);
+fprintf("PROCESSO TERMINATO \n");
 
-%% I robot si fermano a fine traiettoria dal Mitsubishi, rimangono in attesa di ok dal matlab
+%% Il robot si ferma a fine traiettoria dal Mitsubishi, rimane in attesa di ok dal matlab
 tornoacasa=1;%se vuoi che il robot torni in home prima di spegnersi
-M=repmat(num2str(tornoacasa),1,length(addr)-toolvar);%ripeto la stinga di "1" per comunicare con i robot
-if toolvec(end)-toolvar >0 %Se i robot esistono
+if robot1>0 %Se il robot esiste
     str="PARTO";
-    for i=1:toolvec(end)-toolvar %Faccio tornare a Phome i robot
-        temp=MOVE(t(i),M(i),str);
-        fprintf("ROBOT %i : torna a casa \n",i);
-    end
+    temp=MOVE(t(1),num2str(tornoacasa),str);
+    fprintf("ROBOT 1 : torna a casa \n");
 end
 
-%% Salvo i dati in raw format 
+%% Salvo i dati in raw format
 rawdata=data;
 save("rawdata.mat","rawdata");
